6ed1022331d0a2b12da7d4412c4a45f4338876fc
joelrnorris <joel.r.norris@gmail.com>
2018-02-14 17:48:35 -0600

    initial / final commit

---
+++ tipCalcuator/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace tipCalcuator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxNumberOfPeople = new System.Windows.Forms.TextBox();
+            this.textBoxPercentageToTip = new System.Windows.Forms.TextBox();
+            this.textBoxAmountOfBill = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxTotalPerPerson = new System.Windows.Forms.TextBox();
+            this.textBoxTotalToPay = new System.Windows.Forms.TextBox();
+            this.textBoxTipAmount = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.labelWarning = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label13);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.textBoxNumberOfPeople);
+            this.panel1.Controls.Add(this.textBoxPercentageToTip);
+            this.panel1.Controls.Add(this.textBoxAmountOfBill);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(12, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(185, 42);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(15, 13);
+            this.label13.TabIndex = 11;
+            this.label13.Text = "%";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(122, 12);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "$";
+            // 
+            // textBoxNumberOfPeople
+            // 
+            this.textBoxNumberOfPeople.Location = new System.Drawing.Point(135, 65);
+            this.textBoxNumberOfPeople.Name = "textBoxNumberOfPeople";
+            this.textBoxNumberOfPeople.Size = new System.Drawing.Size(50, 20);
+            this.textBoxNumberOfPeople.TabIndex = 5;
+            this.textBoxNumberOfPeople.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxPercentageToTip
+            // 
+            this.textBoxPercentageToTip.Location = new System.Drawing.Point(135, 39);
+            this.textBoxPercentageToTip.Name = "textBoxPercentageToTip";
+            this.textBoxPercentageToTip.Size = new System.Drawing.Size(50, 20);
+            this.textBoxPercentageToTip.TabIndex = 4;
+            this.textBoxPercentageToTip.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxAmountOfBill
+            // 
+            this.textBoxAmountOfBill.Location = new System.Drawing.Point(135, 9);
+            this.textBoxAmountOfBill.Name = "textBoxAmountOfBill";
+            this.textBoxAmountOfBill.Size = new System.Drawing.Size(50, 20);
+            this.textBoxAmountOfBill.TabIndex = 3;
+            this.textBoxAmountOfBill.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Number of People";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Percentage to Tip";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Amount of Bill";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label12);
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.textBoxTotalPerPerson);
+            this.panel2.Controls.Add(this.textBoxTotalToPay);
+            this.panel2.Controls.Add(this.textBoxTipAmount);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Location = new System.Drawing.Point(12, 157);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(122, 73);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(13, 13);
+            this.label12.TabIndex = 10;
+            this.label12.Text = "$";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(122, 44);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 13);
+            this.label11.TabIndex = 9;
+            this.label11.Text = "$";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(122, 16);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 13);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "$";
+            // 
+            // textBoxTotalPerPerson
+            // 
+            this.textBoxTotalPerPerson.Location = new System.Drawing.Point(135, 70);
+            this.textBoxTotalPerPerson.Name = "textBoxTotalPerPerson";
+            this.textBoxTotalPerPerson.Size = new System.Drawing.Size(50, 20);
+            this.textBoxTotalPerPerson.TabIndex = 8;
+            this.textBoxTotalPerPerson.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxTotalToPay
+            // 
+            this.textBoxTotalToPay.Location = new System.Drawing.Point(135, 41);
+            this.textBoxTotalToPay.Name = "textBoxTotalToPay";
+            this.textBoxTotalToPay.Size = new System.Drawing.Size(50, 20);
+            this.textBoxTotalToPay.TabIndex = 7;
+            this.textBoxTotalToPay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBoxTipAmount
+            // 
+            this.textBoxTipAmount.Location = new System.Drawing.Point(135, 13);
+            this.textBoxTipAmount.Name = "textBoxTipAmount";
+            this.textBoxTipAmount.Size = new System.Drawing.Size(50, 20);
+            this.textBoxTipAmount.TabIndex = 6;
+            this.textBoxTipAmount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(10, 73);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 13);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "Total per Person";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Total to Pay";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 16);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Tip Amount";
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Location = new System.Drawing.Point(62, 9);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(96, 13);
+            this.labelTitle.TabIndex = 2;
+            this.labelTitle.Text = "Calculator of TIPS!";
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(12, 129);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalculate.TabIndex = 3;
+            this.buttonCalculate.Text = "Calculate Tip";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(137, 129);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 4;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // labelWarning
+            // 
+            this.labelWarning.AutoSize = true;
+            this.labelWarning.ForeColor = System.Drawing.Color.Crimson;
+            this.labelWarning.Location = new System.Drawing.Point(15, 261);
+            this.labelWarning.Name = "labelWarning";
+            this.labelWarning.Size = new System.Drawing.Size(0, 13);
+            this.labelWarning.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(227, 315);
+            this.Controls.Add(this.labelWarning);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Tip Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBoxAmountOfBill;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.TextBox textBoxNumberOfPeople;
+        private System.Windows.Forms.TextBox textBoxPercentageToTip;
+        private System.Windows.Forms.TextBox textBoxTotalPerPerson;
+        private System.Windows.Forms.TextBox textBoxTotalToPay;
+        private System.Windows.Forms.TextBox textBoxTipAmount;
+        private System.Windows.Forms.Label labelWarning;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ tipCalcuator/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tipCalcuator
+{
+
+    public partial class Form1 : Form
+    {
+
+        double percentageToTip = 0.0;
+        double amountOfBill = 0.0;
+        double numberOfPeople = 0;
+
+        double tipAmount = 0.0;
+        double totalToPay = 0.0;
+        double totalPerPerson = 0.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+
+            // check for input values
+            if (string.IsNullOrEmpty(textBoxAmountOfBill.Text)
+                || string.IsNullOrEmpty(textBoxPercentageToTip.Text) ) {
+
+                // if there's missing values, generate an error message
+                generateUserErrorMessage();
+
+                return;
+
+            }
+
+            // input validatiLOLn, NUMBER ONLY
+            // maybe l8tr
+
+            // if everything's hokay, do some smaths
+
+            // get the input values, which are strings and convert to doubles
+            // some shenenigans here, concatenating the input string w/ a 0. to make in an 
+            // insta~multiplyable floating point deimal
+
+            percentageToTip = Convert.ToDouble( ("0." + textBoxPercentageToTip.Text ) ); 
+            amountOfBill = Convert.ToDouble(textBoxAmountOfBill.Text);
+            numberOfPeople = Convert.ToDouble(textBoxNumberOfPeople.Text);
+
+            tipAmount = amountOfBill * percentageToTip;
+            totalToPay = amountOfBill + tipAmount;
+            totalPerPerson = totalToPay / numberOfPeople;
+
+            // update the outputs
+            textBoxTipAmount.Text = Convert.ToString(tipAmount);
+            textBoxTotalToPay.Text = Convert.ToString(totalToPay);
+            textBoxTotalPerPerson.Text = Convert.ToString(totalPerPerson);
+
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            // clear all text inputs. 
+
+            textBoxAmountOfBill.Text = string.Empty;
+
+            textBoxPercentageToTip.Text = string.Empty;
+
+            textBoxNumberOfPeople.Text = string.Empty;
+
+            textBoxTipAmount.Text = string.Empty;
+
+            textBoxTotalToPay.Text = string.Empty;
+
+            textBoxTotalPerPerson.Text = string.Empty;
+        }
+
+        private void generateUserErrorMessage()
+        {
+            labelWarning.Text = "Hey, you need to fill in all the top three fields";
+        }
+    }
+}
